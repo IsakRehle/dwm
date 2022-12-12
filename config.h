0bf400b68fc7f54b15448baecf7a0d4530499264
Isak Rehle <isak@rehle.xyz>
2022-12-12 09:18:46 +0100

    fixed minor syntax error

--- config.h
+++ config.h
@@ -14,7 +14,7 @@ static const char col_gray1[]       = "#222222";
 /* Inactive window border color */
 static const char col_gray2[]       = "#444444";
 /*  font color  */
-static const char col_gray3[]       = "#bbbbbb"
+static const char col_gray3[]       = "#bbbbbb";
 /* Current tag and current window font color */
 static const char col_gray4[]       = "#eeeeee";
 /* Top bar second color (blue) and active window border color */
