e4b56f1461b6a6d921f2e73d68dc53d6bd11a1ab
Isak Rehle <isak@rehle.xyz>
2023-08-09 15:36:30 +0200

    modified:   push.sh

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# only push the ben branch to my personal remote on github
+# only push the isak branch to my personal remote on github
 # master branch tracks the offical dwm remote
 git push -u github
 
